2cb5fcc68f2c174910dbc76446e16bcc2e085bd7
Clockknight <34818025+Clockknight@users.noreply.github.com>
2021-12-17 12:21:26 -0800

    implemented rarityCases.txt

--- deckboxExporter.py
+++ deckboxExporter.py
@@ -2,192 +2,224 @@ import os
 import sys
 import csv
 import time
-import json
 import requests
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 
-#Global variables
+# Global Variables
 nameDict = {}
 editDict = {}
 condDict = {}
+rareDict = {}
+delay = 4
+failedRows = []
 
-#Global API variables
-with open("./info.txt") as file:
+with open("info.txt") as file:
     lines = file.readlines()
-
-session = requests.session()
-
-api = "bearer " + lines[7][:-1]
-keys = {
-    "Authorization":api,
-    "Content-Type":"application/json"
-}
-url = "https://api.tcgplayer.com/v1.39.0/catalog/categories/1/search"
-page = session.get(url)
-session.headers.update(keys)
-
-def updateBody(name, set):
-    body = {
-        "limit":10000,
-        "filters":[ {
-                        "name": "ProductName",
-                        "values": [name]
-                    },
-                    {
-                        "name": "Set Name",
-                        "values": [set]
-
-                    }, ]
-    }
-    return body
-
-def main():
-    print("Please enter the directory of the file you want to convert.")
-    print("NOTE: PLEASE make sure to follow the readMe instructions!")
-    deckboxDir = input("\t")
-
-    #check to validate it's actually a csv file
-    if not os.path.isfile(deckboxDir) or deckboxDir[-4:] != ".csv":
-        print(deckboxDir[:-4])
-        print("Sorry, that path isn't valid!")
-        sys.exit()
-    else:
-        #Making a file name for converted version
-        convDir = deckboxDir[:-4] + " converted " + time.strftime("%Y%m%d-%H%M%S") + ".csv"
-        convFile = open(convDir, 'w+')
-
-    #Load file in csv Library
-    deckboxRead = csv.reader(open(deckboxDir))
-    deckboxList = list(deckboxRead)
-
-    #Look at Deckbox spreadsheet for LxW parameters
-    dbRows = len(deckboxList)
-    dbCols = 16
-    #Invalid param check
-    if len(deckboxList[0]) != dbCols:
-        print("Sorry, the file provided doesn't have the correct amount of columns! There should be " + str(dbCols))
-
-    #Run edgeDefine for the 3 libraries we need to check
-    edgeDefine()
-
-    #Load file in csv Library
-    deckboxRead = csv.reader(open(deckboxDir))
-    deckboxList = list(deckboxRead)
-    tcgWriter = csv.writer(convFile)
-
-    #Loop through spreadsheet and initialize 2D Array to put values on
-    tcgList = [['TCGplayer Id', 'Product Line', 'Set Name', 'Product Name', 'Title', 'Number', 'Rarity', 'Condition', 'TCG Market Price', 'TCG Direct Low', 'TCG Low Price With Shipping', 'TCG Low Price', 'Total Quantity', 'Add to Quantity', 'TCG Marketplace Price', 'Photo URL']]
-    for y in range(1, dbRows):
-        currRow = []
-        filler = "1" #Filler text to put in columns that don't care about what value is input
-        #TCGplayer ID
-        #We'll get the actual ID later. Leaving this column to fill after all the info is gotten.
-        currRow.append("TODO")
-
-        #Product Line
-        currRow.append("Magic")
-
-        #Set Name
-        cardEdit = edgeCheck(deckboxList[y][3], "Edition")
-        currRow.append(cardEdit)
-
-        #Product Name
-        cardName = edgeCheck(deckboxList[y][2], "Name")
-        currRow.append(cardName)
-
-        #Title
-        currRow.append(filler)
-
-        #Number
-        currRow.append(deckboxList[y][4])
-
-        #Rarity
-        currRow.append(deckboxList[y][15][0].upper())
-
-        #Condition
-        cardCond = edgeCheck(deckboxList[y][5], "Condition")
-        currRow.append(cardCond)
-
-        #TCG Market Price
-        currRow.append(filler)
-
-        #TCG Direct Low
-        currRow.append(filler)
-
-        #TCG Low Price With Shipping
-        currRow.append(filler)
-
-        #TCG Low Price
-        currRow.append(filler)
-
-        #Total Quantity
-        currRow.append(filler)
-
-        #Add to Quantity
-        currRow.append(deckboxList[y][0])
-
-        #TCG Marketplace Price
-        currRow.append(filler)
-
-        #Photo URL
-        currRow.append(filler)
-
-        #Actually get TCG ID
-        body = updateBody(cardName, cardEdit)
-        print(str(y) + " / " + str(dbRows-1), end = "\t")
-        bodyResponse = json.loads(session.post(url, json=body).text)["results"]
-        if(len(bodyResponse) == 1):
-            print("")
-            currRow[0] = bodyResponse[0]
-        elif(len(bodyResponse) > 1):
-            print(" Multiple Response Error")
-            currRow[0] = ""
-        else:
-            print(" No Response Error")
-            currRow[0] = ""
-
-        tcgList.append(currRow)
+    username = lines[0]
+    password = lines[1]
 
 
-    with open(convDir, mode='w') as convFile:
-        convFile = csv.writer(convFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+def managecardlisting(browser, argArray):
+    # Function for card management page, takes array of values (tcgRow) to ADD number of cards to inventory
+    print("managing card")
 
-        for row in tcgList:
-            convFile.writerow(row)
+def edgecheck(dbName, checkType):
+    # Make switch case to open file based on checkType
+    returnString = dbName
 
-def edgeDefine():
-#Then make dict out of file contents
-    with open("nameCases.txt") as file:
-        lines = file.readlines()
-        #Start the array at 2 to check if there is at least 2 lines
-        for i in range(1, len(lines), 2):
-            #Go back to not skip any items
-            #Take away last character since it's a newline character
-            nameDict[lines[i - 1][:-1]] = lines[i][:-1]
-    with open("editionCases.txt") as file:
-        lines = file.readlines()
-        for i in range(1, len(lines), 2):
-            editDict[lines[i - 1][:-1]] = lines[i][:-1]
-    with open("conditionCases.txt") as file:
-        lines = file.readlines()
-        for i in range(1, len(lines), 2):
-            condDict[lines[i - 1][:-1]] = lines[i][:-1]
-
-
-def edgeCheck(dbName, checkType):
-    #Make switch case to open file based on checkType
-    returnName = dbName
-
-    if(checkType=="Edition"):
+    if checkType == "Edition":
         if dbName in editDict:
-            returnName = editDict[dbName]
-    elif(checkType=="Name"):
+            returnString = editDict[dbName]
+    elif checkType == "Name":
         if dbName in nameDict:
-            returnName = nameDict[dbName]
-    elif(checkType == "Condition"):
+            returnString = nameDict[dbName]
+    elif checkType == "Condition":
         if dbName in condDict:
-            returnName = condDict[dbName]
-    #if dbname is in dict, change dbname
-    #else just return it
-    return returnName
+            returnString = condDict[dbName]
+    elif checkType == "Rarity":
+        if dbName in rareDict:
+            returnString = rareDict[dbName]
+    # if dbname is in dict, change dbname
+    # else just return it
+    return returnString
 
-main()
+
+print("Please enter the directory of the file you want to convert.")
+print("NOTE: PLEASE make sure to follow the readMe instructions!")
+deckboxDir = input("\t")
+# Make selenium window
+s = Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=s)
+browser.maximize_window()
+browser.get("https://store.tcgplayer.com/admin/product/catalog")
+wait = WebDriverWait(browser, delay)
+# Log into TCGPlayer
+wait.until(ec.element_to_be_clickable((By.ID, "UserName"))).send_keys(username)
+wait.until(ec.element_to_be_clickable((By.ID, "Password"))).send_keys(password)
+wait.until(ec.element_to_be_clickable((By.ID, "logonButton"))).click()
+# Navigate TCGPlayer after logging in
+# Hard coded to choose option 1 (which is MTG)
+Select(wait.until(ec.element_to_be_clickable((By.ID, "CategoryId")))).select_by_value("1")
+
+# Setup code before looking for cards
+if not os.path.isfile(deckboxDir) or deckboxDir[-4:] != ".csv":
+    print(deckboxDir[:-4])
+    print("Sorry, that path isn't valid!")
+    sys.exit()
+else:
+    # Making a file name for converted version
+    convDir = deckboxDir[:-4] + " converted " + time.strftime("%Y%m%d-%H%M%S") + ".csv"
+    global convFile
+    convFile = open(convDir, 'w+')
+
+# Load file in csv Library
+deckboxRead = csv.reader(open(deckboxDir))
+deckboxList = list(deckboxRead)
+
+# Look at Deckbox spreadsheet for LxW parameters
+dbRows = len(deckboxList)
+dbCols = 16
+# Invalid param check
+if len(deckboxList[0]) != dbCols:
+    print("Sorry, the file provided doesn't have the correct amount of columns! There should be " + str(dbCols))
+
+# define global variables using text files to convert deckbox values into TCGPlayer OK versions
+with open("nameCases.txt") as file:
+    lines = file.readlines()
+    # Start the array at 2 to check if there is at least 2 lines
+    for i in range(1, len(lines), 2):
+        # Go back to not skip any items
+        # Take away last character since it's a newline character
+        nameDict[lines[i - 1][:-1]] = lines[i][:-1]
+with open("editionCases.txt") as file:
+    lines = file.readlines()
+    for i in range(1, len(lines), 2):
+        editDict[lines[i - 1][:-1]] = lines[i][:-1]
+with open("conditionCases.txt") as file:
+    lines = file.readlines()
+    for i in range(1, len(lines), 2):
+        condDict[lines[i - 1][:-1]] = lines[i][:-1]
+with open("rarityCases.txt") as file:
+    lines = file.readlines()
+    for i in range(1, len(lines), 2):
+        rareDict[lines[i - 1][:-1]] = lines[i][:-1]
+
+# Load file in csv Library
+deckboxRead = csv.reader(open(deckboxDir))
+deckboxList = list(deckboxRead)
+tcgWriter = csv.writer(convFile)
+
+# Loop through spreadsheet and initialize 2D Array to put values on
+tcgList = [['Set Name', 'Product Name', 'Number', 'Rarity', 'Condition', 'Total Quantity']]
+for y in range(1, dbRows):
+    currRow = []
+
+    # Set Name
+    cardEdit = edgecheck(deckboxList[y][3], "Edition")
+    currRow.append(cardEdit)
+
+    # Product Name
+    cardName = edgecheck(deckboxList[y][2], "Name")
+    currRow.append(cardName)
+
+    # Number
+    currRow.append(deckboxList[y][4])
+
+    # Rarity
+    currRow.append(edgecheck(deckboxList[y][15], "Rarity"))
+
+    # Condition
+    currRow.append(edgecheck(deckboxList[y][5], "Condition"))
+
+    # Add to Quantity
+    currRow.append(deckboxList[y][0])
+
+    tcgList.append(currRow)
+
+with open(convDir, mode='w') as convFile:
+    convFile = csv.writer(convFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for row in tcgList:
+        convFile.writerow(row)
+
+convReadFile = open(convDir, mode='r')
+
+# Choose Set name from csv
+time.sleep(delay/2)
+SetNameDropdown = browser.find_element(By.ID, 'SetNameId')
+
+#Define HTML elements like the textbox that dont change
+textbox = browser.find_element(By.ID, "SearchValue")
+button = browser.find_element(By.ID, "Search")
+
+# Use tcgList and for loop to search through using already converted data
+# this loops once for each row/listing in the array
+
+# Order of variables
+# Set Name
+# Product Name
+# Number
+# Rarity
+# Condition
+# Add to Quantity
+
+for tcgRow in tcgList[1:]:
+    # reset variables
+    matchBoolean = False
+
+    # clear textbox to avoid accidental matches
+    textbox.clear()
+
+    # navigate search function using tcgRow values
+    Select(SetNameDropdown).select_by_visible_text(tcgRow[0])
+    # wait inbetween inputs to let page process
+    time.sleep(delay/2)
+    print(tcgRow[3])
+    Select(browser.find_element(By.ID, "Rarity")).select_by_visible_text(tcgRow[3])
+    time.sleep(delay/2)
+    textbox.send_keys(str(tcgRow[1]))
+    button.click()
+    time.sleep(delay/2)
+
+    # If the current url is still https://store.tcgplayer.com/admin/product/catalog
+    if browser.current_url == "https://store.tcgplayer.com/admin/product/catalog":
+        # Find table
+        listingTable = browser.find_element(By.ID, "ProductsTable").get_attribute('innerHTML')
+        # find tds inside tbody
+        soup = BeautifulSoup(listingTable, "html.parser")
+        tableRows = soup.find("tbody").find_all("tr")
+        # for each td
+        for row in tableRows:
+            # create array for the row based on each td
+            rowArray = []
+            for item in row.find_all("td"):
+                rowArray.append(item.get_text().strip())
+
+            # compare name to make sure name is not a partial match
+            if rowArray[1] == tcgRow[1]:
+                # compare against number
+                if rowArray[4] == tcgRow[2]:
+                    # if both values match parameters run managecardlisting and break for loop
+                    managecardlisting(browser, tcgRow)
+                    matchBoolean = True
+                    break
+
+        # If there's no more tds, and no match was found
+        if not matchBoolean:
+            # Put row in a new array for writing errors
+            failedRows.append(tcgRow)
+        # At the end of the program, write the array to a failed csv
+
+    # else, run managecardlisting
+    else:
+        managecardlisting(browser, tcgRow)
+
+# write failed rows to csv
