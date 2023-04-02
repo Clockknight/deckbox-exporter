d9ba6613e68ef10cbc62fcebaf4dd4923f332eac
Clockknight <hello@tylerhanyinwang.com>
2023-04-01 23:41:53 -0700

    Import all cases from txt files to dictionary in python file

---
+++ cases.py
@@ -0,0 +1,38 @@
+class tcgplayerCases():
+
+
+    Condition = {
+        "Mint":"Near Mint",
+        "Good (Lightly Played)":"Lightly Played",
+        "Played":"Moderately Played",
+        "Heavily Played":"Heavily Played",
+        "Poor":"Damaged"
+    }
+
+
+    Edition = {
+        "Magic 2010":"Magic 2010 (M10)",
+        "Magic 2011":"Magic 2011 (M11)",
+        "Magic 2012":"Magic 2012 (M12)",
+        "Magic 2013":"Magic 2013 (M13)",
+        "Magic 2014":"Magic 2014 (M14)",
+        "Magic 2015":"Magic 2015 (M15)",
+        "Modern Masters 2015 Edition":"Modern Masters 2015",
+        "Prerelease Events: Throne of Eldraine":"Prerelease Events"
+    }
+
+
+    Name = {
+
+    }
+
+
+
+    Rarity = {
+        "MythicRare" : "Mythic",
+        "BasicLand" : "Land"
+    }
+
+
+
+
